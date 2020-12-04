816aa732d3c2593f036b70e2d451106dac81faae
patriciamae003 <54754183+patriciamae003@users.noreply.github.com>
2020-12-03 22:49:53 -0500

    Update Login.java

--- src/restaurant/system/Login.java
+++ src/restaurant/system/Login.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package restaurant.system;
 
 import javax.swing.JOptionPane;
 import java.sql.*;
 
-/**
- *
- * @author patri
- */
 public class Login extends javax.swing.JFrame {
 
     /**
@@ -159,10 +150,6 @@ public class Login extends javax.swing.JFrame {
     private void createAccountBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_createAccountBtnMouseClicked
         //
     }//GEN-LAST:event_createAccountBtnMouseClicked
-
-    /**
-     * @param args the command line arguments
-     */
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
